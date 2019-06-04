569e687c0a230b77df1356b35352309861fb6280
github username <github email address>
2019-06-04 16:02:13 -0600

    Added backend JWT BCrypt Auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_194357) do
+ActiveRecord::Schema.define(version: 2019_06_04_210036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_194357) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
   add_foreign_key "teams", "users"
