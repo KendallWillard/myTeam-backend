6326e9fb1d6aa9b3db89fdde7b5f88d30559d4d1
github username <github email address>
2019-06-12 11:24:25 -0600

    removed password digest from serilaizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :phone, :username, :password_digest
+  attributes :id, :first_name, :last_name, :phone, :username
 end
